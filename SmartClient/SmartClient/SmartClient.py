eefc40e3a99ec225842379dfcb17539a78d6c33d
oghodwhy <moena94@hotmail.com>
2018-01-22 17:07:01 -0800

    Created main.
    Added main.

---
+++ SmartClient/SmartClient/SmartClient.py
@@ -0,0 +1,19 @@
+import sys
+
+
+
+verbose = True
+
+
+def main(argv):
+    if (verbose): 
+        print ('initiating main')
+
+
+
+    sys.exit
+    
+    
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
