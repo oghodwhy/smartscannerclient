99f155978ab6c8b22ac276d0d27ee3e755323194
oghodwhy <moena94@hotmail.com>
2018-01-24 21:06:59 -0800

    Skeleton
    Created the basic outline for function of the simple client including option handling.

--- SmartClient/SmartClient/SmartClient.py
+++ SmartClient/SmartClient/SmartClient.py
@@ -1,16 +1,67 @@
 import sys
-
-
-
-verbose = True
-
+import getopt
+import socket
+import ssl
+import http
 
 def main(argv):
-    if (verbose): 
-        print ('initiating main')
+
+    #starting params
+    verbose = False
+    connects = False
+    hasSSL = False
+    targetAddr = '' 
+    HTTPversion = ''
+
+    #Get input options
+    try:
+        opts, args = getopt.getopt(argv,"v")
+    except getopt.GetoptError:
+        print ('Error with input options.\n Try in the form SmartClient.py %targetip%\n Use -h for help.\n')
+        sys.exit(2)
+
+    #Check input options
+    for param in opts:
+
+        #help option
+        if param == '-h':
+            print ('basic use with SmartClient.py %targetip%')
+            print ('Use -v for verbose mode.\n')
+            sys.exit()
+        
+        #verbose option
+        if param == '-v':
+            verbose = True
+            print ('Starting client in verbose mode.\n')
+
+    if (verbose): print ('Initiating main...')
+
+    #Check input format
+    if (verbose): print ('Checking input address...')
 
 
-
+    #Create socket
+    if (verbose): print ('Creating socket...')
+    
+    
+    #Attempt to connect socket
+    if (verbose): print ('Attempting to connect to target...')
+    
+    
+    #Check if HTTPS
+    if (verbose): print ('Checking for HTTPS...')
+    
+    
+    #Check HTTP version
+    if (verbose): print ('Checking HTTP version...')
+    
+    
+    #Print Result
+    if (verbose): print ('Printing results...')
+    
+    
+    
+    if (verbose): print ("Job's Done!")
     sys.exit
     
     
